72195879e92bb4d6f18694dd2ce0f1fdda715782
JanekKondzior <kondziorjanek@protonmail.com>
2018-10-08 16:32:35 +1100

    On branch master

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php include('inc/head.php');
-require_once('conn.php');
+include('conn.php');
 include('members.php');
 include('income.php');
 include('expences.php');
@@ -7,19 +7,12 @@ include('expences.php');
     
     <div class="container">
         <div class="buttons">
-        <form action="<?php $_SERVER['PHP_SELF']?>" method="post">
-            <button name="populate">Populate database</button>
-        
-        
-            <button name="members">Paid Members</button>
-        
-        
-            <button name="income">Total Income Received</button> 
-     
-        
-            <button name="expences">Total expenses incurred</button>
-        </form>
-
+            <form action="<?php $_SERVER['PHP_SELF']?>" method="post">
+                <button name="populate">Populate database</button>
+                <button name="members">Paid Members</button>
+                <button name="income">Total Income Received</button> 
+                <button name="expences">Total expenses incurred</button>
+            </form>
         </div>
     </div>
 
