5f46ee3e2b477913faa1d01cd9b5920def15dc6c
Isaac <isaac.g.hawkins@gmail.com>
2018-10-08 16:30:41 +1100

    asdasd

--- index.php
+++ index.php
@@ -1,14 +1,7 @@
 <?php include('inc/head.php');
 require_once('conn.php');
-<<<<<<< HEAD
 ?>
-=======
-include('members.php');
-include('income.php');
-include('expences.php');
- ?>
-    
->>>>>>> a0f0c0545a66d96a89d3c4ff7c39f15dcac99c10
+
     <div class="container">
         <div class="buttons">
         <form action="<?php $_SERVER['PHP_SELF']?>" method="post">
