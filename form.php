d9ab4ee199e4c3f7111faa5b7252b120dd24a945
JanekKondzior <kondziorjanek@protonmail.com>
2018-10-13 15:33:04 +1100

    form scripts

--- form.php
+++ form.php
@@ -2,26 +2,9 @@
 <?php
     include('inc/head.php');
     include('conn.php');
+    include('form_member.php');
+    include('form_payment.php');
 ?>
 
 
-// Enrollment form for members
 
-<form action="" method="post">
-    <div>
-        <label for="firstName">First Name</label>
-        <input type="text" name="firstName">
-    </div>
-    <div>
-        <label for="lastName">Last Name</label>
-        <input type="text" name="firstName">
-    </div>
-    <div>
-        <label for="parentFirst">Parent First Name</label>
-        <input type="text" name="parentFirst">
-    </div>
-    <div>
-        <label for="parentLast">Parent Last Name</label>
-        <input type="text" name="parentLast">
-    </div>
-</form>
---
+++ form_member.php
@@ -0,0 +1,20 @@
+// Enrollment form for members 
+
+<form action="" method="post">
+    <div>
+        <label for="firstName">First Name</label>
+        <input type="text" name="firstName">
+    </div>
+    <div>
+        <label for="lastName">Last Name</label>
+        <input type="text" name="firstName">
+    </div>
+    <div>
+        <label for="parentFirst">Parent First Name</label>
+        <input type="text" name="parentFirst">
+    </div>
+    <div>
+        <label for="parentLast">Parent Last Name</label>
+        <input type="text" name="parentLast">
+    </div>
+</form>
